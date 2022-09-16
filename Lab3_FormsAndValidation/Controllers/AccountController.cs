f38872099455d908443543847727ae69adfe850e
Anuj Aggarwal <anuj010787@gmail.com>
2022-09-16 20:38:24 +0530

    Lab3 ASP.NET Core 6 Forms and validation

---
+++ Lab3_FormsAndValidation/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using Lab3_FormsAndValidation.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace Lab3_FormsAndValidation.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(UserModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //To Do:
+                TempData["Message"] = "Your account has been created!";
+                //TempData["Name"] = model.Name;
+
+                //string strData = JsonSerializer.Serialize(model);
+              //
+                return RedirectToAction("Message");
+            }
+            return View();
+        }
+
+        public IActionResult Message()
+        {
+            //string message = (string)TempData["Message"]; //read once
+            //string name = (string)TempData.Peek("Name"); //read many
+
+
+            return View();
+        }
+    }
+}
